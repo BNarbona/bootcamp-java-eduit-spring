fc89431b7bc44bc16d211516df46560376316a57
BNarbona <banarbona@gmail.com>
2022-08-26 20:25:22 -0300

    creacion dev

--- src/main/java/ar/com/educacionit/controllers/HomeController.java
+++ src/main/java/ar/com/educacionit/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //@Repository
-
+//prueba
 @Controller
 public class HomeController {
 
