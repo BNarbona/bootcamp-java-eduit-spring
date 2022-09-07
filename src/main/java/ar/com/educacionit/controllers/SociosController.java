90397b906cb610f39b1aeb7b8b97cd75c461482f
BNarbona <banarbona@gmail.com>
2022-09-07 17:03:51 -0300

    Funcion editar y eliminar

--- src/main/java/ar/com/educacionit/controllers/SociosController.java
+++ src/main/java/ar/com/educacionit/controllers/SociosController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ar.com.educacionit.domain.Socios;
 import ar.com.educacionit.services.SociosService;
@@ -27,4 +29,23 @@ public class SociosController {
 		model.addAttribute("socios", socios);
 		return "socios";
 	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam(name="idSocio", required= true) Long idSocio) {
+		
+		this.sociosService.eliminar(idSocio);
+		
+		return "redirect:/socio/all";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String preEdit(
+			@PathVariable(name="id", required= true) Long id,
+			Model model){
+		
+		Socios socios = this.sociosService.buscarSocio(id);
+		model.addAttribute("socio", socios);
+		return "edit";
+	}
+
 }
