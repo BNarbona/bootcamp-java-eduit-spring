d546ddfb6e3c81223b5493ca95d5109f6ff79b9c
BNarbona <banarbona@gmail.com>
2022-09-15 18:16:23 -0300

    Log +validacion

--- src/main/java/ar/com/educacionit/controllers/UploadController.java
+++ src/main/java/ar/com/educacionit/controllers/UploadController.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 
 import javax.servlet.http.Part;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +19,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.LoggingEvent;
+
 
 @Controller
 @RequestMapping("/upload")
 public class UploadController {
 
+	Logger logger = (Logger) LoggerFactory.getLogger("");
 	
 	@PostMapping("/up")
 		public String uploadFile(
@@ -35,7 +41,11 @@ public class UploadController {
 				return "index";
 			}
 			
-			String fileName = file.getName();
+			if(!this.validar(file)) {
+				model.addAttribute("error", "");
+				model.addAttribute("message", "Por favor seleccione un archivo con extension .txt o .csv");
+				return "index";
+			}
 			
 			//creo el path para guardar el archivo 
 			StringBuilder builder = new StringBuilder();
@@ -48,8 +58,16 @@ public class UploadController {
 			Path path = Paths.get(builder.toString());
 			Files.write(path, fileBytes);
 			
+			logger.info("File upload Name: " + file.getOriginalFilename()+"| Size: "+file.getSize()+" bytes");
 			model.addAttribute("message", "Archivo cargado correctamente en la ruta: "+builder.toString());
 			return "index";
 		}
 		
+		public static boolean validar (MultipartFile file) {
+			String name = file.getOriginalFilename();
+			String [] arrayName = name.split("\\.");
+			String ext = arrayName[arrayName.length-1]; 
+			return ext.equalsIgnoreCase("csv")||ext.equalsIgnoreCase("txt");
+		}
 }
+
